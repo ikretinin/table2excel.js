7b166a2a4f70ef93280dcefce9038211895a97fd
GMCS/ikretinin <ikretinin@gmcs.ru>
2018-07-25 11:29:24 +0300

    r

--- src/table2excel.js
+++ src/table2excel.js
@@ -57,7 +57,7 @@ export default class Table2Excel {
         this._invokePlugin('workbookCreated', { workbook, tables })
 
         tables.forEach((table, index) => {
-            if (!!this.names) {
+            if (!this.names) {
                 const worksheet = workbook.addWorksheet(`Sheet ${index + 1}`);
                 console.log('if: ', worksheet);
             } else {
